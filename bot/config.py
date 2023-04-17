acec94f5dc77662c0235b46fa2a013c33a17ee52
Shreecontent01 <130931406+Shreecontent01@users.noreply.github.com>
2023-04-18 04:11:26 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -12,19 +12,22 @@
 #
 # License can be found in <
 # https://github.com/1Danish-00/CompressorQueue/blob/main/License> .
-
 from decouple import config
-
 try:
-    APP_ID = "7868296", cast=int
-    API_HASH = "bfa7fecb33443243ff8c8b0f27f9cd68"
-    BOT_TOKEN = "5896665703:AAEoqSzLV7l5BxlImlHyEXEBDKM1NY2aQ_4"
-    DEV = 5012158248
-    OWNER = 5012158248
-    ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By AnshuSharma (https://github.com/Anshusharma75/TG-videoCompress)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMB = "https://te.legra.ph/file/9c245e214cfdadf167e13.jpg"
+    APP_ID = config("APP_ID", default=7868296, cast=int)
+    API_HASH = config("API_HASH", default="bfa7fecb33443243ff8c8b0f27f9cd68")
+    BOT_TOKEN = config("BOT_TOKEN", default="5896665703:AAEoqSzLV7l5BxlImlHyEXEBDKM1NY2aQ_4")
+    DEV = 1322549723
+    OWNER = config("OWNER", default=5012158248 )
+    FFMPEG = config(
+        "FFMPEG",
+        default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
+    )
+    THUMB = config(
+        "THUMBNAIL", default="https://te.legra.ph/file/9c245e214cfdadf167e13.jpg"
+    )
 except Exception as e:
-    LOGS.info("Environment vars Missing")
-    LOGS.info("something went wrong")
-    LOGS.info(str(e))
-    exit(1)
+    print("Environment vars Missing")
+    print("something went wrong")
+    print(str(e))
+    exit()
